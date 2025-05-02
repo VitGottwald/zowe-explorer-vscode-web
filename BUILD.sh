de73e76bb9c1f09cb7e2dac60e47f2f7f9d1d3d3
Vit Gottwald <vit.gottwald@broadcom.com>
2025-05-02 15:08:24 +0200

    Build with pnpm

--- BUILD.sh
+++ BUILD.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 
-function fcho() {
+function fecho() {
   echo '* ----------------------------------------------'
   echo "$*"
   echo '* ----------------------------------------------'
 }
 
 # Build dependencies in the `zowe-cli` module
-fcho '* Entering zowe-cli submodule'
+fecho '* Entering zowe-cli submodule'
 cd zowe-cli
-fcho '* Installing zowe-cli node_modules'
-npm ci
-fcho '* Building zowe-cli'
-npm run build
+fecho '* Installing zowe-cli node_modules'
+pnpm install --frozen-lockfile
+fecho '* Building zowe-cli'
+pnpm run build
 cd ..
 
 # Build zowe-explorer-api
-fcho '* Entering zowe-explorer-vscode submodule'
+fecho '* Entering zowe-explorer-vscode submodule'
 cd zowe-explorer-vscode
-fcho '* Installing zowe-explorer-vscode node_modules'
+fecho '* Installing zowe-explorer-vscode node_modules'
 pnpm install --frozen-lockfile
-fcho '* Building zowe-explorer-vscode'
+fecho '* Building zowe-explorer-vscode'
 pnpm run build
 cd ..
 
 # Build zowe-explorer web bundle
-fcho '* Entering zowe-explorer package'
+fecho '* Entering zowe-explorer package'
 cd zowe-explorer-vscode/packages/zowe-explorer
-fcho '* Building zowe-explorer web bundle'
+fecho '* Building zowe-explorer web bundle'
 pnpm run build:web
 cd ../../..
 
 # Start the extension in browser
-fcho '* Starting zowe-explorer web bundle in browser'
+fecho '* Starting zowe-explorer web bundle in browser'
 pnpm install --frozen-lockfile
 pnpm run start
--- README.md
+++ README.md
@@ -2,21 +2,25 @@
 
 A repo to tie together matching commits of ZOWE Explorer and ZOWE CLI (a dependency of ZOWE Explorer) and build them into a VS Code WEB compatible extension.
 
-## Dependencies
-
-- nodejs 22.14.0
-- npm 10.9.2
-- pnpm 10.8.1
-
 ## Clone
 
 ``` sh
 git clone --recurse-submodules https://github.com/VitGottwald/zowe-explorer-vscode-web.git
-```
-
-## Build
-
-```
 cd zowe-explorer-vscode-web
+```
+
+## Dependencies
+
+- pnpm 10.8.1
+
+To install it, run
+
+```
+curl -fsSL https://get.pnpm.io/install.sh | env PNPM_VERSION=10.8.1 sh -
+```
+
+## Build and run
+
+```
 ./BUILD.sh
 ```
